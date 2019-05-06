02f8f2dec7f4f751107b30ebfb99198f59da7156
monatno1987 <50119640+monatno1987@users.noreply.github.com>
2019-05-06 10:49:32 -0400

    Update TestApplication.java

--- src/main/java/com/test/test/TestApplication.java
+++ src/main/java/com/test/test/TestApplication.java
@@ -2,12 +2,7 @@ package com.test.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @ComponentScan({"com.test.test","com.test.controller"})
 @SpringBootApplication
